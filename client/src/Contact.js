f016c39c636093faf05765d17030984563e1643f
ADITHYAADHI06 <adishettygu102@gmail.com>
2022-11-30 23:02:35 +0530

    implemention of contact page

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,12 +1,78 @@
-import React from 'react'
+import styled from "styled-components";
 
 const Contact = () => {
+    const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container{
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Wrapper>
+            <h2 className="common-heading">Contact page</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d74719.08904262018!2d74.69497915976768!3d13.617539029113571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbc912ae3b16e37%3A0x2fecb6210917fad7!2sUVA%20MERIDIAN%20AMUSEMENT%20PARK!5e0!3m2!1sen!2sin!4v1667928638197!5m2!1sen!2sin" width="100%" height="400" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <div className="container">
+                <div className="contact-form">
+                    <form
+                        action="https://formspree.io/f/xoqbolnz"
+                        method="POST"
+                        className="contact-inputs">
+                        <input
+                            type="text"
+                            placeholder="username"
+                            name="username"
+                            required
+                            autoComplete="off"
+                        />
 
-    )
-}
+                        <input
+                            type="email"
+                            name="Email"
+                            placeholder="Email"
+                            autoComplete="off"
+                            required
+                        />
 
-export default Contact
+                        <textarea
+                            name="Message"
+                            cols="30"
+                            rows="10"
+                            required
+                            autoComplete="off"
+                            placeholder="Enter you message"></textarea>
+
+                        <input type="submit" value="send" />
+                    </form>
+                </div>
+            </div>
+        </Wrapper>
+    );
+};
+
+export default Contact;
