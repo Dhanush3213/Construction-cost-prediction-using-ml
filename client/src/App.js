b21dbf459beb35b37b818abec48d66d9bcafa807
Dhanush <dhanusha311@gmail.com>
2023-03-08 22:11:44 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ErrorPage from "./ErrorPage";
 import Login from "./Pages/Login";
+import CostEstimation from "./CostEstimation";
 
 
 const App = () => {
@@ -55,6 +56,7 @@ const App = () => {
           <Route path="/singleProject/:id" element={<SingleProject />}></Route>
           <Route path="/wishlist" element={<Wishlist />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          <Route path="/costestimation" element={<CostEstimation />}></Route>
 
           <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
